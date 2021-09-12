be1d2f06a58333045aa2dd913370c644565ab047
root <root@DESKTOP-E0U416F.localdomain>
2021-09-12 18:58:24 -0400

    More

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -6,6 +6,7 @@ from project.server.models import User
 
 auth_blueprint = Blueprint('auth', __name__)
 
+
 class RegisterAPI(MethodView):
     """
     User Registration Resource
@@ -20,7 +21,8 @@ class RegisterAPI(MethodView):
 
     def post(self):
         # get the post data
-        post_data = request.get_json(); print(request)
+        post_data = request.get_json()
+        print(request)
         # check if user already exists
         user = User.query.filter_by(email=post_data.get('email')).first()
         if not user:
@@ -38,7 +40,7 @@ class RegisterAPI(MethodView):
                 responseObject = {
                     'status': 'success',
                     'message': 'Successfully registered.',
-                    'auth_token': auth_token.decode()
+                    'auth_token': auth_token
                 }
                 return make_response(jsonify(responseObject)), 201
             except Exception as e:
@@ -55,12 +57,43 @@ class RegisterAPI(MethodView):
             return make_response(jsonify(responseObject)), 202
 
 
+class UserAPI(MethodView):
+    """
+    User Registration Resource
+    """
+
+    def get(self):
+        responseObject = {
+            'status': 'success',
+            'message': 'Request successful but please send an HTTP POST request to register the user.'
+        }
+        return make_response(jsonify(responseObject)), 201
+
+    def post(self):
+        # get the post data
+        post_data = request.get_json()
+        print(request)
+        # check if user already exists
+        myUsers = db.session.query(User).all
+        myReturnedUsers = {}
+        for user in myUsers:
+            myReturnedUsers[user.id] = user.email
+        return myReturnedUsers
+
+
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
+user_view = UserAPI.as_view('user_api')
 
 # add Rules for API Endpoints
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
+    methods=['POST', 'GET']
+)
+
+auth_blueprint.add_url_rule(
+    '/users/index',
+    view_func=user_view,
     methods=['POST', 'GET']
 )
